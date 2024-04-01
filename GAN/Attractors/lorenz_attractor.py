7daa7b0a61d978a4bf8e0444b7e4302eb4d86f90
Brenno Natal <natalbrenno@gmail.com>
2024-04-01 19:11:36 -0300

    default initial state and params

--- GAN/Attractors/lorenz_attractor.py
+++ GAN/Attractors/lorenz_attractor.py
@@ -9,6 +9,25 @@ class LorenzAttractor(BaseChaoticAttractor):
     that define the Lorenz attractor.
     """
 
+    DEFAULT_SIGMA = 10.0
+    DEFAULT_RHO = 28.0
+    DEFAULT_BETA = 8.0 / 3.0
+    DEFAULT_INITIAL_STATE = [1.1, 2.0, 7.0]
+
+    def __init__(self, initial_state=None, sigma=None, rho=None, beta=None):
+        """
+        Initialize the Lorenz attractor with default or specified initial state and parameters.
+        """
+        if initial_state is None:
+            initial_state = self.DEFAULT_INITIAL_STATE
+        if sigma is None:
+            sigma = self.DEFAULT_SIGMA
+        if rho is None:
+            rho = self.DEFAULT_RHO
+        if beta is None:
+            beta = self.DEFAULT_BETA
+        super().__init__(initial_state, (sigma, rho, beta))
+
     def next_state(self, t, state, parameters):
         """
         Compute the next state of the Lorenz system given the current state and time.
